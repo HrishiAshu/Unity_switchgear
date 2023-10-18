a96f82864f42d82dc4c4f4faed0498cec771502c
Hrishikeshsonawane <hrishikeshsonawane9@gmail.com>
2023-10-19 01:15:54 +0530

    completed homepage

--- src/Components/Main/Layout/Header/Footer.jsx
+++ src/Components/Main/Layout/Header/Footer.jsx
@@ -3,39 +3,45 @@ import './Footer.css'
 import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
- 
+
 
 const Footer = () => {
   return (
-    <footer style={{padding: "20px 20px 20px 20px"}} className="bg-dark text-light">
-    <Container>
-      <Row>
-        <Col md={4}>
-          <h4>Company Name</h4>
-          <p>A brief description of company .</p>
-        </Col>
-        <Col md={4}>
-          <h4>Links</h4>
-          <ul className="list-unstyled">
-            <li><a href="/">Home</a></li>
-            <li><a href="/about">About</a></li>
-            <li><a href="/products">Products</a></li>
-            <li><a href="/contact">Contact</a></li>
-          </ul>
-        </Col>
-        <Col md={4}>
-          <h4>Contact</h4>
-          <p>Email: contact@example.com</p>
-          <p>Phone: +1 (123) 456-7890</p>
-        </Col>
-      </Row>
-      <Row>
-        <Col className="text-center">
-          <p>&copy; {new Date().getFullYear()}  Company</p>
-        </Col>
-      </Row>
-    </Container>
-  </footer>
+    <>
+      <footer style={{ padding: "20px 20px 20px 20px" }} className="bg-dark text-light">
+        <Container>
+          <Row>
+            <Col md={4}>
+              <h4>Unity Switchgear</h4>
+              <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quos, nam..</p>
+            </Col>
+            <Col md={4}>
+              <h4>Links</h4>
+              <ul className="list-unstyled">
+                <li><a href="/">Home</a></li>
+                <li><a href="/about">About</a></li>
+                <li><a href="/products">Products</a></li>
+                <li><a href="/contact">Contact</a></li>
+              </ul>
+            </Col>
+            <Col md={4}>
+              <h4>Contact</h4>
+              <p>Email: contact@example.com</p>
+              <p>Phone: +1 (123) 456-7890</p>
+            </Col>
+          </Row>
+          <Row>
+            <Col className="text-center">
+              <p>&copy; {new Date().getFullYear()}  Company</p>
+            </Col>
+          </Row>
+        </Container>
+
+      </footer>
+      <p style={{textAlign:"center"}}>  Unity © 2023. All Rights Reserved.</p>
+
+    <p style={{textAlign:"center"}}>Developed By Hrishikesh</p>  
+    </>
   )
 }
 
--- src/Components/Main/Layout/Header/Header.jsx
+++ src/Components/Main/Layout/Header/Header.jsx
@@ -5,45 +5,54 @@ import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import NavDropdown from 'react-bootstrap/NavDropdown';
 import 'bootstrap/dist/css/bootstrap.css';
+import { useNavigate } from 'react-router-dom';
 
 
 const Header = () => {
+
+    const navigate = useNavigate();
+
+const handleWhyUnity=()=>{
+    navigate("./WhyUnity")
+
+}
+
+
     return (
         <div className='header-main-container'>
             <div className="headerContent">
-
-
-                <Navbar expand="lg" className="bg-body-tertiary fixed-top" >
+                <Navbar expand="lg" className="bg-body-tertiary fixed-top">
                     <Container>
                         <Navbar.Brand href="#home">unity</Navbar.Brand>
                         <Navbar.Toggle aria-controls="basic-navbar-nav" />
                         <Navbar.Collapse id="basic-navbar-nav">
                             <Nav className="me-auto">
-                                <Nav.Link href="#home">Home</Nav.Link>
-                                <Nav.Link href="#link">About Us</Nav.Link>
-                                <Nav.Link href="#link">Products</Nav.Link>
-                                <Nav.Link href="#link">Contact Us</Nav.Link>
+                                <Nav.Link href="/">Home</Nav.Link>
+                                <NavDropdown title="About Us" id="basic-nav-dropdown">
+                                    <NavDropdown.Item onClick={handleWhyUnity} href="/WhyUnity/">Why Unity</NavDropdown.Item>
+                                    <NavDropdown.Item href="#action/3.2">Our History</NavDropdown.Item>
+                                    <NavDropdown.Item href="#action/3.3">Our Customers</NavDropdown.Item>
+                                    {/* <NavDropdown.Item href="#action/3.3">Our Customers</NavDropdown.Item>
+                                    <NavDropdown.Item href="#action/3.3">Our Customers</NavDropdown.Item> */}
 
+
+                                </NavDropdown>
+                                <Nav.Link href="#link">Products</Nav.Link>
+                                <Nav.Link href="#link">Solutions</Nav.Link>
+                                <Nav.Link href="#link">Projects</Nav.Link>
+
+
+                                <Nav.Link href="#link">Contact Us</Nav.Link>
                             </Nav>
                             <NavDropdown title="Profile" id="basic-nav-dropdown">
-
                                 <NavDropdown.Item href="/Admin/*">Admin</NavDropdown.Item>
                                 <NavDropdown.Divider />
-
-                                <NavDropdown.Item href="#action/3.2">
-                                    Another action
-                                </NavDropdown.Item>
+                                <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
                                 <NavDropdown.Divider />
-
                                 <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
                                 <NavDropdown.Divider />
-
-                                <NavDropdown.Item href="#action/3.4">
-                                    Separated link
-                                </NavDropdown.Item>
-
+                                <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
                             </NavDropdown>
-
                         </Navbar.Collapse>
                     </Container>
                 </Navbar>
